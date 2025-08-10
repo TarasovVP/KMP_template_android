5f7f2284d369ea9e08e40ddddb39aff94f1dff81
TarasovVP <vptarasov1998@gmail.com>
2025-08-10 12:24:43 +0300

    Add shared dependencies

--- app/src/main/java/com/vnteam/kmp_template_android/MainActivity.kt
+++ app/src/main/java/com/vnteam/kmp_template_android/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tarasovvp.kmptemplate.SystemInfo
 import com.vnteam.kmp_template_android.ui.theme.KMP_template_androidTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +24,10 @@ class MainActivity : ComponentActivity() {
             KMP_template_androidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        name = SystemInfo().getInfo(),
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
@@ -32,10 +37,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        Text(text = name)
+    }
 }
 
 @Preview(showBackground = true)
